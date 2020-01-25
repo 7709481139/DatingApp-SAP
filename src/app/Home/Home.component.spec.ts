4b50177f2c9a73a65201eda9757a2626b4964b31
vishal <narwade.v.v@gmail.com>
2020-01-25 18:13:36 +0530

    Client side register method

--- src/app/Home/Home.component.spec.ts
+++ src/app/Home/Home.component.spec.ts
@@ -3,21 +3,21 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { ValuesComponent } from './Values.component';
+import { HomeComponent } from './Home.component';
 
-describe('ValuesComponent', () => {
-  let component: ValuesComponent;
-  let fixture: ComponentFixture<ValuesComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ValuesComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ValuesComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/Home/Home.component.ts
+++ src/app/Home/Home.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
+import {HttpClient} from '@angular/common/http';
 @Component({
-  selector: 'app-Values',
-  templateUrl: './Values.component.html',
-  styleUrls: ['./Values.component.css']
+  selector: 'app-Home',
+  templateUrl: './Home.component.html',
+  styleUrls: ['./Home.component.css']
 })
-export class ValuesComponent implements OnInit {
-   values: any;
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  values: any;
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getValues();
   }
-
+  registerToggle()  {
+    this.registerMode = !this.registerMode;
+  }
   getValues()
   {
     this.http.get('http://localhost:5000/values/').subscribe(response => {
